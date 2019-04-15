dd3677dd7102ee110feafbdfdd56c2ce252b01c1
FOBkid20 <38386222+FOBkid20@users.noreply.github.com>
2019-04-14 22:23:18 -0500

    add relations list to details page and better format

--- screens/Details/Details.js
+++ screens/Details/Details.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationActions } from 'react-navigation';
-import { Item, } from 'native-base';
+import { List, ListItem, Item, Left, Right} from 'native-base';
 import CustomHeader from '../../components/Common/CustomHeader'
 
 import { connect } from 'react-redux';
+
+import {
+    thunk_load_details,
+} from '../../ducks/details';
 
 import styles from './styles';
 
@@ -37,8 +41,16 @@ class DetailsComponent extends Component {
           
             action: NavigationActions.navigate({ routeName: 'AddRelations' }),
         });
+
         
-        // TODO: change spaces to two different columns
+        const toDetails = NavigationActions.navigate({
+            routeName: 'Details',
+          
+            params: {},
+          
+            action: NavigationActions.navigate({ routeName: 'Details' }),
+        });
+        
         return (
             <View style={flatListStyle}>
                 <Text style = {{fontWeight: 'bold', textAlign: 'center', fontSize: 40, paddingTop: 10}}>
@@ -47,18 +59,58 @@ class DetailsComponent extends Component {
                 <Text style = {{ color: '#DB9872', textAlign: 'center', fontSize: 20, paddingBottom: 25,}}>
                     {this.state.relation}
                 </Text>
-                <Text style = {{textAlign: 'left', fontSize: 24}}>
-                    Birth Year&nbsp;&nbsp;&nbsp;&nbsp;  
-                    {this.state.birth_year}
+                <View style={{flex: 1, flexDirection: 'row',}}>
+                    <View style={{flex: 1, flexDirection: 'column',}}>
+                        <Text style = {{textAlign: 'left', fontSize: 24, color: 'grey'}}>
+                            Birth Year
+                        </Text>
+                        <Text style = {{textAlign: 'left', fontSize: 24, color: 'grey'}}>
+                            Nickname
+                        </Text>
+                        <Text style = {{textAlign: 'left', fontSize: 24, color: 'grey'}}>
+                            Lives in
+                        </Text>
+                    </View>
+                    <View style={{flex: 1, flexDirection: 'column',}}>  
+                        <Text style = {{textAlign: 'left', fontSize: 24}}>
+                            {this.state.birth_year}
+                        </Text>
+                        <Text style = {{textAlign: 'left', fontSize: 24}}>
+                            {this.state.nickname}
+                        </Text>
+                        <Text style = {{textAlign: 'left', fontSize: 24}}>
+                            {this.state.lives_in}
+                        </Text>
+                    </View>
+                </View>
+
+                // need to make relations list directly follow under lives_in section
+                
+                <Text style = {{textAlign: 'left', fontSize: 24, color: 'grey'}}>
+                    Relations
                 </Text>
-                <Text style = {{textAlign: 'left', fontSize: 24}}>
-                    Nickname&nbsp;&nbsp;&nbsp;&nbsp;
-                    {this.state.nickname}
-                </Text>
-                <Text style = {{textAlign: 'left', fontSize: 24}}>
-                    Lives in&nbsp;&nbsp;&nbsp;&nbsp;
-                    {this.state.lives_in}
-                </Text>
+                <List>
+                    <ListItem onPress={() => this.props.navigation.dispatch(toDetails)}>
+                        <Left><Text style={{fontSize: 20}}>Amy Yang</Text></Left> 
+                        <Right><Text style={{fontWeight: "bold", color: '#DB9872'}}>Me</Text></Right>
+                    </ListItem>
+                    <ListItem onPress={() => this.props.navigation.dispatch(toDetails)}>
+                        <Left><Text style={{fontSize: 20}}>Lexi Ryan</Text></Left> 
+                        <Right><Text style={{fontWeight: "bold", color: '#DB9872'}}>Mother</Text></Right>
+                    </ListItem>
+                    <ListItem onPress={() => this.props.navigation.dispatch(toDetails)}>
+                        <Left><Text style={{fontSize: 20}}>Lexi Ryan</Text></Left> 
+                        <Right><Text style={{fontWeight: "bold", color: '#DB9872'}}>Mother</Text></Right>
+                    </ListItem>
+                    <ListItem onPress={() => this.props.navigation.dispatch(toDetails)}>
+                        <Left><Text style={{fontSize: 20}}>Lexi Ryan</Text></Left> 
+                        <Right><Text style={{fontWeight: "bold", color: '#DB9872'}}>Mother</Text></Right>
+                    </ListItem>
+                    <ListItem onPress={() => this.props.navigation.dispatch(toDetails)}>
+                        <Left><Text style={{fontSize: 20}}>Lexi Ryan</Text></Left> 
+                        <Right><Text style={{fontWeight: "bold", color: '#DB9872'}}>Mother</Text></Right>
+                    </ListItem>
+                </List>
                 <Button
                     title="go to AddRelations"
                     onPress={() => this.props.navigation.dispatch(toAddRelations)}
